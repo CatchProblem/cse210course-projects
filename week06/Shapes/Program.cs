daf8a63a69ac647707a12d48ef79240c18534bdc
Opio Moses <catchproblems00@gmail.com>
2025-06-11 09:49:43 +0300

    Add Shape, Square, Rectangle, and Circle classes with area calculation and testing in the Shapes project

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Test individual shapes
+        Square square = new Square("Red", 5);
+        Console.WriteLine($"Square color: {square.GetColor()}, area: {square.GetArea()}");
+
+        Rectangle rectangle = new Rectangle("Blue", 4, 6);
+        Console.WriteLine($"Rectangle color: {rectangle.GetColor()}, area: {rectangle.GetArea()}");
+
+        Circle circle = new Circle("Green", 3);
+        Console.WriteLine($"Circle color: {circle.GetColor()}, area: {circle.GetArea()}");
+
+        // Polymorphism in action: List<Shape>
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        Console.WriteLine("\nList of shapes:");
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape color: {shape.GetColor()}, area: {shape.GetArea()}");
+        }
     }
 }
